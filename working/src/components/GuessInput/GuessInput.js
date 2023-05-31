51492246375b2d361031de190fe56bb9eaa20f24
Alek Novkovski <alek@greatcode2.com>
2023-05-31 16:38:39 +0200

    On submit, clear input, console-log value

--- working/src/components/GuessInput/GuessInput.js
+++ working/src/components/GuessInput/GuessInput.js
@@ -3,8 +3,14 @@ import React, {useState} from "react";
 function GuessInput() {
     const [inputValue, setInputValue] = useState('');
 
+    function submitHandler(event) {
+        event.preventDefault();
+        setInputValue('')
+        console.log(inputValue);
+    }
+
     return (
-        <form className="guess-input-wrapper">
+        <form className="guess-input-wrapper" onSubmit={(event) => submitHandler(event)}>
             <label htmlFor="guess-input">Enter guess:</label>
             <input
                 id="guess-input"
