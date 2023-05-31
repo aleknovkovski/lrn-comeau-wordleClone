e6aa03a2c2a026db3c1996850ff9d69e9ac174ef
Alek Novkovski <alek@greatcode2.com>
2023-05-31 16:32:03 +0200

    Double-bind input, uppercase on input

--- working/src/components/GuessInput/GuessInput.js
+++ working/src/components/GuessInput/GuessInput.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function GuessInput() {
+    const [inputValue, setInputValue] = useState('');
+
     return (
         <form className="guess-input-wrapper">
             <label htmlFor="guess-input">Enter guess:</label>
-            <input id="guess-input" type="text"/>
+            <input
+                id="guess-input"
+                type="text"
+                value={inputValue}
+                onChange={(event) => {
+                    setInputValue(event.target.value.toUpperCase())
+                }}
+            />
         </form>
     );
 }
