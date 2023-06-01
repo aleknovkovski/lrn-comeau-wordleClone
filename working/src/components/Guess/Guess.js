63c9a8076f8a6cff3094fa0838078e6052e92ebc
Alek Novkovski <alek@greatcode2.com>
2023-06-01 11:00:22 +0200

    Prettier reformat

--- working/src/components/Guess/Guess.js
+++ working/src/components/Guess/Guess.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Guess({word}) {
 
-  return (
-      <p className="guess">
-        <span className="cell">{word?.[0]}</span>
-        <span className="cell">{word?.[1]}</span>
-        <span className="cell">{word?.[2]}</span>
-        <span className="cell">{word?.[3]}</span>
-        <span className="cell">{word?.[4]}</span>
-      </p>
-  );
+    return (
+        <p className="guess">
+            <span className="cell">{word?.[0]}</span>
+            <span className="cell">{word?.[1]}</span>
+            <span className="cell">{word?.[2]}</span>
+            <span className="cell">{word?.[3]}</span>
+            <span className="cell">{word?.[4]}</span>
+        </p>
+    );
 }
 
 export default Guess;
