8650c8bc722dab8dacbec61e1ae5623864265972
Alek Novkovski <alek@greatcode2.com>
2023-06-01 10:09:12 +0200

    Create Guess Component, Render Guesses With

--- working/src/components/GuessesList/GuessesList.js
+++ working/src/components/GuessesList/GuessesList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Guess from "../Guess";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants";
+import {range} from "../../utils";
 
 function GuessesList({guesses}) {
   return (
       <div className="guess-results">
-        {guesses.map((guess, index) => <p className="guess" key={index}>{guess}</p>)}
+          {range(0, NUM_OF_GUESSES_ALLOWED, 1).map((index) => {
+            return <Guess word={guesses[index]} key={index}/>
+          })}
       </div>
   );
 }
