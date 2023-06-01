1c2a2b5565f03e44da43a856b931414a5841b3d9
Alek Novkovski <alek@greatcode2.com>
2023-06-01 12:21:53 +0200

    Track stats, show banner on win or loss

--- working/src/components/Game/Game.js
+++ working/src/components/Game/Game.js
@@ -4,6 +4,7 @@ import {sample} from '../../utils';
 import {WORDS} from '../../data';
 import GuessInput from "../GuessInput";
 import GuessesList from "../GuessesList";
+import Banner from "../Banner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,15 +13,23 @@ console.info({answer});
 
 
 function Game() {
+    const[status, setStatus] = React.useState('playing');
     const [guesses, setGuesses] = React.useState([]);
 
     const handleNewGuess = (submittedGuess) => {
         setGuesses((prevGuesses) => [...prevGuesses, submittedGuess]);
+
+        if (submittedGuess === answer) {
+            setStatus('won');
+        } else if (guesses.length >= 5) {
+            setStatus('lost');
+        }
     }
 
     return <>
         <GuessesList guesses={guesses} answer={answer}/>
         <GuessInput handleNewGuess={handleNewGuess}/>
+        <Banner status={status} guesses={guesses.length} answer={answer}/>
     </>;
 }
 
